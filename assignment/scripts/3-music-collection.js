f035308871e3b2016008ff6f5578a9ae2a8d681c
Jose Rubio <ru310z@gmail.com>
2021-06-04 21:55:47 -0500

    addCollection test added

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,3 +11,11 @@ function addCollection( title, artist, yearPublished ){
   collection.push(album);
   return album;
 }
+
+console.log(addCollection('Finisterra', 'Mago de oz', '2000'));
+console.log(addCollection('Beerserkers', 'Lepoka', '2016'));
+console.log(addCollection('No Grave But the Sea', 'Alestorm', '2017'));
+console.log(addCollection('Karkelo', 'Korpiklaani', '2009'));
+console.log(addCollection('Buiikikaesu', 'Maximum the Hormone', '2007'));
+console.log(addCollection(`The Warrior's Code`,'Dropkick Murphys', '2005'));
+console.log(collection);
