6290d1e73ef5c607cbbf300dfa5b4b904c8cb0f5
Jose Rubio <ru310z@gmail.com>
2021-06-04 21:39:09 -0500

    Function addCollection added

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,13 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addCollection( title, artist, yearPublished ){
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  };
+  collection.push(album);
+  return album;
+}
