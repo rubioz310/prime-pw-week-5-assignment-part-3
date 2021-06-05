fb7c5ae98519fafb073479b04b1bd2f26c0fef38
Jose Rubio <ru310z@gmail.com>
2021-06-04 22:07:18 -0500

    added showCollection function plus comments

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,7 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
-
+//Function that adds an album to the collection
 function addCollection( title, artist, yearPublished ){
   let album = {
     title: title,
@@ -11,7 +11,7 @@ function addCollection( title, artist, yearPublished ){
   collection.push(album);
   return album;
 }
-
+//Test's for addCollection function
 console.log(addCollection('Finisterra', 'Mago de oz', '2000'));
 console.log(addCollection('Beerserkers', 'Lepoka', '2016'));
 console.log(addCollection('No Grave But the Sea', 'Alestorm', '2017'));
@@ -19,3 +19,12 @@ console.log(addCollection('Karkelo', 'Korpiklaani', '2009'));
 console.log(addCollection('Buiikikaesu', 'Maximum the Hormone', '2007'));
 console.log(addCollection(`The Warrior's Code`,'Dropkick Murphys', '2005'));
 console.log(collection);
+// Function that list all albums in the collection
+function showCollection(thisCollection){
+  console.log(thisCollection.length);
+  for(let thisAlbum of thisCollection){
+    console.log(`${thisAlbum.title} by ${thisAlbum.artist}, published in ${thisAlbum.yearPublished}`);
+  }
+}
+//Calling previous function
+showCollection(collection);
